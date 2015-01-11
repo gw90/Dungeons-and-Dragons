abc7bad972715c6088919a7a40ae00e152384b97
gw90 <gw90@users.noreply.github.com>
2015-01-11 16:35:06 -0500

    Create character.js

---
+++ character.js
@@ -0,0 +1,10 @@
+var name = "Zeno Onez";
+var class = "rouge";
+var race = "elf"
+var level = 1;
+var alignment = "unaligned";
+var languages = ["common", "elven"];
+//first number value, second number modifier
+var strength = [10, 4];
+//boolean is trained? int is modifier
+var athletics = [False, 4];
